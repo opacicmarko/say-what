877f40d1089b5e683984148c95e5fb7e9b2af0c3
Marko Opačić <98588282+opacicmarko@users.noreply.github.com>
2025-09-24 14:39:49 +0200

    sane defaults, improved ease of use

--- say.sh
+++ say.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-sleep 5
+# Check if we're already running under nohup (detect parent process or use a flag)
+if [ -z "$ALREADY_NOHUP" ]; then
+  export ALREADY_NOHUP=1
+  nohup "$0" "$@" > /dev/null 2>&1 &
+  exit
+fi
+
+# Defaults
+INITIAL_TIMEOUT=60
+MESSAGE="ha ha ha, your computer has been compromised"
+
+# Overwrite defaults if arguments are provided
+if [ "$#" -eq 1 ]; then
+	MESSAGE=$1
+	echo $1
+elif [ "$#" -eq 2 ]; then
+	MESSAGE=$1
+	INITIAL_TIMEOUT=$2
+fi
+
+sleep $INITIAL_TIMEOUT
 
 while true; do
 	osascript -e "set volume output volume 100"
-	say "bro"
+	say $MESSAGE
 	sleep 1
 done
